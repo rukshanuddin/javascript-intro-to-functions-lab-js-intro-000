c02af6fe26b4df48c321dbcdc9019668a5e3ebcd
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-09-17 17:03:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function shout(string){
   return string.toUpperCase()
 }
 function whisper(string){
-  return string.toLowerCase
+  return string.toLowerCase()
 }
-expect(whisper('HELLO')).toEqual('hello')
+
