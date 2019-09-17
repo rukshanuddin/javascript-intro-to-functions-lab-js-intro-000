64368284a237bc1395fda3aec2788afdad137de3
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-09-17 17:13:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function whisper(string){
 function logShout(string){
   return console.log(string.toUpperCase())
 }
+function logWhisper(string{
+  return console.log(string.toLowerCase())
+})
+function sayHiToGrandma(string){
+  if string.toLowerCase === string {return "I can't hear you!"}
+}
